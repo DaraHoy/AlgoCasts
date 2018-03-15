7f9690af60c0cf29c121430af45964a7950dc730
Dara <darahoy@gmail.com>
2018-03-15 11:34:19 -0400

    Add solution to maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let char = {};
+    for(char of str){
+        if (!char[char]){
+            char[char] = 1;
+        } else {
+            char[char]++;
+        }
+    }
+    return char;
+}
 
 module.exports = maxChar;
