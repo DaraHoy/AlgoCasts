5f594d6920400d47734fbbad49dfbbd0f93a51e4
Dara <darahoy@gmail.com>
2018-03-20 13:56:58 -0400

    Add first solution to anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,7 +7,46 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+// HINT: regEx exclusion rule: word.replace(/[^\w]/g, "").toLowerCase()
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  //helper function to clean both strings and return an object of keys
+  let charMap = function(str) {
+    let map = {};
+    let word = str.replace(/[^\w]/g, "").toLowerCase();
+
+    for (char of word) {
+      if (!map[char]) {
+        map[char] = 1;
+      } else {
+        map[char]++;
+      }
+    }
+    return map;
+  };
+
+  let mapA = charMap(stringA);
+  let mapB = charMap(stringB);
+
+  if (Object.keys(mapA).length === Object.keys(mapB).length) {
+    //checks if mapA has the same quantity of each characters in mapB, and vice versa
+    for (key in mapA) {
+      if (mapA[key] === mapB[key]) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+    for (key in mapB) {
+      if (mapA[key] === mapB[key]) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,15 +9,15 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    //Create array to hold chunks 
-    const arr = [];
-    //Divide chunks into subarrays by size 
-    while(array.length > size){
-        arr.push(array.splice(0, size));
-    }
-    //Add remaining unchunkable items into their own array  
-    arr.push(array.splice(0));
-    return arr 
+  //Create array to hold chunks
+  const arr = [];
+  //Divide chunks into subarrays by size
+  while (array.length > size) {
+    arr.push(array.splice(0, size));
+  }
+  //Add remaining unchunkable items into their own array
+  arr.push(array.splice(0));
+  return arr;
 }
 
 module.exports = chunk;
