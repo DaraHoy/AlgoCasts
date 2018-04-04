ab219249da48bff9d4d0c9759b50658a10a17abc
Dara <darahoy@gmail.com>
2018-04-04 10:45:04 -0400

    Add solution to stair

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  //iterate through row
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    //iterate through column
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
